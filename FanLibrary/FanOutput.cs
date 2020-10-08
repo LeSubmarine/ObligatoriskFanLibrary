dbb4b2920d2ae756485a96fee37345ba96295dd5
LeSubmarine <henrik@osterby.dk>
2020-10-09 00:48:13 +0200

    DoneLmao

---
+++ FanLibrary/FanOutput.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace FanLibrary
+{
+    public class FanOutput
+    {
+        private string _name = "";
+        private double _temperature;
+        private double _humidity; 
+
+        /// <summary>
+        /// Class FanOutput is for keeping track of a given fan.
+        /// </summary>
+        /// <param name="id">
+        ///     id is the given id of the fan.
+        /// </param>
+        /// <param name="name">
+        ///     name is the given name of the fan.
+        ///     The name parameter must be a string of at least to characters.
+        /// </param>
+        /// <param name="temperature">
+        ///     temperature is the current temperature of the fan.
+        ///     The temperature parameter is given in celsius, and must greater or equal than 15 and less or equal to 25.
+        /// </param>
+        /// <param name="humidity">
+        ///     humidity is the current humidity of the fan.
+        ///     The humidity parameter is given in relative humidity, and must greater or equal than 30 and less or equal to 80.
+        /// </param>
+        public FanOutput(int id, string name, double temperature, double humidity)
+        {
+            Id = id;
+            Name = name;
+            Temperature = temperature;
+            Humidity = humidity;
+        }
+        /// <summary>
+        ///     Empty Contructor for json use etc.
+        /// </summary>
+        public FanOutput()
+        {
+            
+        }
+        /// <summary>
+        ///     Id is the given id of the fan.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///     Name is the given name of the fan.
+        ///     The Name property must be a string of at least to characters.
+        /// </summary>
+        public string Name
+        {
+            get { if (_name.Length >= 2) { return _name; } else { throw new FormatException("The Name property is not initialized"); } }
+            set { if(value.Length >= 2){_name = value;} else {throw new FormatException($"The value {value} is not a valid value for the Name property.\nThe Name property must be a string of at least to characters.");} }
+        }
+        /// <summary>
+        ///     Temperature is the current temperature of the fan.
+        ///     The Temperature property is given in celsius, and must greater or equal than 15 and less or equal to 25.   
+        /// </summary>
+        public double Temperature
+        {
+            get { if (_temperature >= 15 && _temperature <= 25) {return _temperature;} else{ throw new FormatException("The Temperature property is not initialized");} }
+            set { if (value >= 15 && value <= 25) { _temperature = value; } else { throw new FormatException($"The value {value} is not a valid value for the Temperature property.\nThe Temperature property is given in celsius, and must greater or equal than 15 and less or equal to 25."); } }
+        }
+
+        /// <summary>
+        ///     Humidity is the current humidity of the fan.
+        ///     The Humidity property is given in relative humidity, and must greater or equal than 30 and less or equal to 80.
+        /// </summary>
+        public double Humidity
+        {
+            get { if (_humidity >= 30 && _humidity <= 80) { return _humidity; } else { throw new FormatException("The Humidity property is not initialized"); } }
+            set { if (value >= 30 && value <= 80) { _humidity = value; } else { throw new FormatException($"The value {value} is not a valid value for the Humidity property.\nThe Humidity property is given in relative humidity, and must greater or equal than 30 and less or equal to 80."); } }
+        }
+    }
+}
---
+++ FanLibraryUnitTest/FanOutputUnitTest.cs
@@ -0,0 +1,176 @@
+using System;
+using FanLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FanLibraryUnitTest
+{
+    [TestClass]
+    public class FanOutputUnitTest
+    {
+        private FanOutput _fanOutput;
+        private int _testId;
+        private string _testName;
+        private double _testTemp;
+        private double _testHumidity;
+
+        [TestInitialize]
+        public void preTest()
+        {
+            _fanOutput = null;
+            _testId = 0;
+            _testName = "Id test";
+            _testTemp = 20;
+            _testHumidity = 40;
+        }
+
+        [TestMethod]
+        public void TestContructor()
+        {
+            _fanOutput = new FanOutput();
+            _fanOutput = new FanOutput(_testId, _testName, _testTemp, _testHumidity);
+            Assert.AreEqual(_testId,_fanOutput.Id);
+            Assert.AreEqual(_testName,_fanOutput.Name);
+            Assert.AreEqual(_testTemp, _fanOutput.Temperature);
+            Assert.AreEqual(_testHumidity,_fanOutput.Humidity);
+        }
+
+        [TestMethod]
+        public void TestId()
+        {
+            _fanOutput = new FanOutput();
+            _fanOutput.Id = -9999;
+            _fanOutput.Id = 0;
+            _fanOutput.Id = 9999;
+            _fanOutput = new FanOutput(0,"Id test",20,40);
+            _fanOutput.Id = -9999;
+            _fanOutput.Id = 0;
+            _fanOutput.Id = 9999;
+        }
+        [TestMethod]
+        public void TestName()
+        {
+            string nameCheck;
+            _fanOutput = new FanOutput();
+            try
+            {
+                nameCheck = _fanOutput.Name;
+                Assert.Fail();
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            _fanOutput = new FanOutput();
+            try
+            {
+                _fanOutput.Name = "";
+                Assert.Fail();
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            _fanOutput = new FanOutput();
+            _fanOutput.Name = "Test";
+            Assert.AreEqual("Test",_fanOutput.Name);
+
+            _fanOutput = new FanOutput(_testId,_testName,_testTemp,_testHumidity);
+            _fanOutput.Name = "NewTest";
+            Assert.AreEqual("NewTest",_fanOutput.Name);
+        }
+        [TestMethod]
+        public void TestTemperature()
+        {
+            double tempCheck;
+            _fanOutput = new FanOutput();
+            try
+            {
+                tempCheck = _fanOutput.Temperature;
+                Assert.Fail();
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            _fanOutput = new FanOutput();
+            try
+            {
+                _fanOutput.Temperature = 0.00;
+                Assert.Fail();
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            _fanOutput = new FanOutput();
+            _fanOutput.Temperature = 16;
+            Assert.AreEqual(16, _fanOutput.Temperature);
+
+            _fanOutput = new FanOutput(_testId, _testName, _testTemp, _testHumidity);
+            Assert.AreEqual(_testTemp, _fanOutput.Temperature, 0.001);
+
+            _fanOutput.Temperature = 17.4;
+            Assert.AreEqual(17.4, _fanOutput.Temperature,0.001);
+
+            try
+            {
+                _fanOutput.Temperature = 25.00001;
+                Assert.Fail();
+            }
+            catch (FormatException e)
+            {
+
+            }
+        }
+        [TestMethod]
+        public void TestHumidity()
+        {
+            double humidityCheck;
+            _fanOutput = new FanOutput();
+            try
+            {
+                humidityCheck = _fanOutput.Humidity;
+                Assert.Fail();
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            _fanOutput = new FanOutput();
+            try
+            {
+                _fanOutput.Humidity = 0.00;
+                Assert.Fail();
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            _fanOutput = new FanOutput();
+            _fanOutput.Humidity = 37;
+            Assert.AreEqual(37, _fanOutput.Humidity);
+
+            _fanOutput = new FanOutput(_testId, _testName, _testTemp, _testHumidity);
+            Assert.AreEqual(_testHumidity, _fanOutput.Humidity, 0.001);
+
+            _fanOutput.Humidity = 37.4;
+            Assert.AreEqual(37.4, _fanOutput.Humidity, 0.001);
+
+            try
+            {
+                _fanOutput.Humidity = 80.00001;
+                Assert.Fail();
+            }
+            catch (FormatException e)
+            {
+
+            }
+        }
+    }
+}
